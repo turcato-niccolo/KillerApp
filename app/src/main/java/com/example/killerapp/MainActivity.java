75d43d23e63b2af87fafd378a82a351fc8a3c97a
Niccolo' Turcato <56435959+turcato-niccolo@users.noreply.github.com>
2019-11-06 11:10:05 +0100

    Development Branch created

--- app/src/main/java/com/example/killerapp/MainActivity.java
+++ app/src/main/java/com/example/killerapp/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public final String ACCESS_FINE_LOCATION = "android.permission.ACCESS_FINE_LOCATION";
+    public final String ACCESS_COARSE_LOCATION = "android.permission.ACCESS_COARSE_LOCATION";
+    public final String ACCESS_BACKGROUND_LOCATION = "android.permission.ACCESS_BACKGROUND_LOCATION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

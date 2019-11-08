18a524c990aa32dd805140ec4a542781e7e3acd1
Niccolo' Turcato <56435959+turcato-niccolo@users.noreply.github.com>
2019-11-08 09:58:01 +0100

    Added Permission Request

--- app/src/main/java/com/example/killerapp/MainActivity.java
+++ app/src/main/java/com/example/killerapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.killerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -11,6 +16,10 @@ public class MainActivity extends AppCompatActivity implements SmsHandler.OnSmsE
     public final String ACCESS_FINE_LOCATION = "android.permission.ACCESS_FINE_LOCATION";
     public final String ACCESS_COARSE_LOCATION = "android.permission.ACCESS_COARSE_LOCATION";
     public final String ACCESS_BACKGROUND_LOCATION = "android.permission.ACCESS_BACKGROUND_LOCATION";
+    public final String SEND_SMS = "android.permission.SEND_SMS";
+    public final String RECEIVE_SMS = "android.permission.RECEIVE_SMS";
+    public final String READ_SMS = "android.permission.READ_SMS";
+    public final int REQUEST_CODE_SMS = 1;
 
     private SmsHandler smsHandler;
 
@@ -23,13 +32,21 @@ public class MainActivity extends AppCompatActivity implements SmsHandler.OnSmsE
         smsHandler.registerReceiver(getApplicationContext(), true, false, false);
         smsHandler.setListener(this);
 
+        requestSmsPermission();
+    }
+
+    public void requestSmsPermission()
+    {
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) +
+                ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS))
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{SEND_SMS, RECEIVE_SMS, READ_SMS}, REQUEST_CODE_SMS);
+        }
     }
 
     /***
-     * This method is called when the app is closed, by the owner of the device with another instance
-     * of this application
-     * It gets the geo-loc and triggers a sound to be played, then sends an sms to answer the sender
-     * that contains the position of the device
+     *
      * @param message Received SMSMessage class of SmsHandler library
      */
     @Override

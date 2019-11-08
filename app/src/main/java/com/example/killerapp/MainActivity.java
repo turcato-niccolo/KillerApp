7826b56f7f96cca966c6320320ed0b2c90b8d3a7
Niccolo' Turcato <56435959+turcato-niccolo@users.noreply.github.com>
2019-11-08 14:58:19 +0100

    Fixed library usage, application was previously using the specific class, now using common interface CommunicationHandler through SMSManager.

--- app/src/main/java/com/example/killerapp/MainActivity.java
+++ app/src/main/java/com/example/killerapp/MainActivity.java
@@ -9,10 +9,12 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.dezen.riccardo.smshandler.ReceivedMessageListener;
 import com.dezen.riccardo.smshandler.SMSMessage;
-import com.dezen.riccardo.smshandler.SmsHandler;
 
-public class MainActivity extends AppCompatActivity implements SmsHandler.OnSmsEventListener {
+import com.dezen.riccardo.smshandler.SMSManager;
+
+public class MainActivity extends AppCompatActivity implements ReceivedMessageListener<SMSMessage> {
     public final String ACCESS_FINE_LOCATION = "android.permission.ACCESS_FINE_LOCATION";
     public final String ACCESS_COARSE_LOCATION = "android.permission.ACCESS_COARSE_LOCATION";
     public final String ACCESS_BACKGROUND_LOCATION = "android.permission.ACCESS_BACKGROUND_LOCATION";
@@ -21,16 +23,15 @@ public class MainActivity extends AppCompatActivity implements SmsHandler.OnSmsE
     public final String READ_SMS = "android.permission.READ_SMS";
     public final int REQUEST_CODE_SMS = 1;
 
-    private SmsHandler smsHandler;
+    private SMSManager manager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        smsHandler = new SmsHandler();
-        smsHandler.registerReceiver(getApplicationContext(), true, false, false);
-        smsHandler.setListener(this);
+        manager = manager.getInstance(getApplicationContext());
 
         requestSmsPermission();
     }
@@ -49,22 +50,10 @@ public class MainActivity extends AppCompatActivity implements SmsHandler.OnSmsE
      *
      * @param message Received SMSMessage class of SmsHandler library
      */
-    @Override
-    public void onReceive(SMSMessage message)
+    public  void onMessageReceived(SMSMessage message)
     {
         //TODO add specific functionality -> Feature branch
         Toast.makeText(getApplicationContext(), "Message Received",Toast.LENGTH_LONG).show();
     }
 
-    @Override
-    public void onSent(int resultCode, SMSMessage message)
-    {
-        //TODO? probably
-    }
-
-    @Override
-    public void onDelivered(int resultCode, SMSMessage message)
-    {
-        //TODO? Is it necessary? the other client should as well send a confirmation
-    }
 }

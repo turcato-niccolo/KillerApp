56ab64c9c59926adf20a2c502fa2c31b6e6252b1
deep <pardeep.kumar@studenti.unipd.it>
2019-11-09 13:02:07 +0100

    added gps coordinates

--- app/src/main/java/com/example/killerapp/MainActivity.java
+++ app/src/main/java/com/example/killerapp/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -22,7 +25,13 @@ import com.dezen.riccardo.smshandler.SmsHandler;
 public class MainActivity extends AppCompatActivity implements SmsHandler.OnSmsEventListener {
     private EditText phoneNumber;
     private Button sendButton;
+    private EditText gpsLatitude;
+    private EditText gpsLongitude;
     private String messaggeConstant="ciao";
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private final int REQUEST_GPS_LOCATION=1;
+    private final int REQUEST_GPS_LOCATION2=1;
 
 
     private static final String[] permissions = {
@@ -40,6 +49,8 @@ public class MainActivity extends AppCompatActivity implements SmsHandler.OnSmsE
         setContentView(R.layout.activity_main);
         phoneNumber=findViewById(R.id.phoneNumber);
         sendButton=findViewById(R.id.sendButton);
+        gpsLatitude=findViewById(R.id.gpsLatitude);
+        gpsLongitude=findViewById(R.id.gpsLongitude);
 
         smsHandler = new SmsHandler();
         smsHandler.registerReceiver(getApplicationContext(), true, false, false);
@@ -57,6 +68,7 @@ public class MainActivity extends AppCompatActivity implements SmsHandler.OnSmsE
     {
         smsHandler.sendSMS(this, destination, messaggeConstant);
     }
+    //request permissions to get gps coordinates and to send sms
     public void requestPermissions()
     {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)+
@@ -77,9 +89,42 @@ public class MainActivity extends AppCompatActivity implements SmsHandler.OnSmsE
         AudioManager audioManager= (AudioManager) getSystemService((Context.AUDIO_SERVICE));
         audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),0);
         mediaPlayer.start();
-        //TODO add specific functionality -> Feature branch
-        Toast.makeText(getApplicationContext(), "Message Received",Toast.LENGTH_LONG).show();
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener= new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        if (((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) + ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)) !=PackageManager.PERMISSION_GRANTED))
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_GPS_LOCATION );
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_GPS_LOCATION2 );
+        }
+        locationManager.requestLocationUpdates("gps", 10000, 0, locationListener);
+        Location location=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        gpsLatitude.append("\n "+location.getLatitude());
+        gpsLongitude.append("\n "+location.getLongitude());
+
+
+
     }
+
 
     @Override
     public void onSent(int resultCode, SMSMessage message)

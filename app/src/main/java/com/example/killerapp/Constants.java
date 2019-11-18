da25b21eb236a1e1f4ffab7c79c762cd8544062d
Niccolo' Turcato <56435959+turcato-niccolo@users.noreply.github.com>
2019-11-18 09:21:25 +0100

    File for code review
    Reviewers expected: Scialpi, Ursino

--- app/src/main/java/com/example/killerapp/Constants.java
+++ app/src/main/java/com/example/killerapp/Constants.java
@@ -4,6 +4,8 @@ package com.example.killerapp;
  * Contains the constants that need to be shared between activities of the application,
  * all of them are defined package-private
  */
+
+//Reviewers expected: Scialpi, Ursino
 class Constants {
     final String[] locationMessages = {"LOCATION_REQUEST", "LOCATION_RESPONSE"};
     final String[] audioAlarmMessages = {"AUDIO_ALARM_REQUEST", "AUDIO_ALARM_RESPONSE"};
